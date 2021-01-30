b3956920c35325277a7a964b1a4c9e634b6e7b81
jsjuris89 <jsjuris89@gmail.com>
2021-01-30 10:02:41 +0200

    make Form a controlled component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import Form from "./components/Form";
 
+const dramasList = [];
+
+const formInitialState = {
+  drama: "",
+  poster: "",
+  startDate: "",
+  endDate: "",
+  genres: [],
+  episodes: "",
+  plot: "",
+  mainCast: "",
+  country: "",
+  director: "",
+  screenwriter: "",
+};
+
 function App() {
+  console.log("[APP.JS] running....");
+  const [formState, setFormState] = useState(formInitialState);
+
   return (
     <div className="App">
-      <Form></Form>
+      <Form
+        formState={formState}
+        setFormState={setFormState}
+        dramas={dramasList}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,70 +1,164 @@
-import React from "react";
 import styles from "./Form.module.css";
 
-const Form = () => {
+const Form = ({ formState, setFormState, dramas }) => {
+  console.log("[FORM.JS] running....");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dramas.push(formState);
+    console.log("dramas:", dramas);
+  };
+
+  const handleInput = (e) => {
+    const inputName = e.currentTarget.name;
+    const value = e.currentTarget.value;
+
+    setFormState((prev) => ({ ...prev, [inputName]: value }));
+  };
+
+  const handleCheckboxInput = (e) => {
+    const inputName = e.currentTarget.name;
+    const value = e.currentTarget.value;
+
+    // if checkbox is checked we add a new genre to genres array
+    if (e.currentTarget.checked) {
+      setFormState((prev) => ({
+        ...prev,
+        [inputName]: [...prev[inputName], value],
+      }));
+    } else {
+      setFormState((prev) => ({
+        ...prev,
+        [inputName]: prev[inputName].filter((genre) => value !== genre), // otherwise we remove that genre from genres array
+      }));
+    }
+  };
+
   return (
     <div>
       <h1>Add a drama</h1>
-      <form className={styles.formContainer}>
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
         <div className={styles.labelContainer}>
           <label>Drama name</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="drama"
+            value={formState.drama}
+            onChange={handleInput}
+          />
         </div>
 
         <div className={styles.labelContainer}>
           <label>Poster link</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="poster"
+            value={formState.poster}
+            onChange={handleInput}
+          />
         </div>
 
         <div className={styles.labelContainer}>
           <label>Start Date</label>
-          <input type="date" />
+          <input
+            type="date"
+            name="startDate"
+            value={formState.startDate}
+            onChange={handleInput}
+          />
           <label>End Date</label>
-          <input type="date" />
+          <input
+            type="date"
+            name="endDate"
+            value={formState.endDate}
+            onChange={handleInput}
+          />
         </div>
 
         <label>Genres:</label>
         <div className={styles.checkboxFlex}>
           <label>Comedy</label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            name="genres"
+            value="comedy"
+            onChange={handleCheckboxInput}
+          />
         </div>
         <div className={styles.checkboxFlex}>
           <label>Romance</label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            name="genres"
+            value="romance"
+            onChange={handleCheckboxInput}
+          />
         </div>
         <div className={styles.checkboxFlex}>
           <label>School</label>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            name="genres"
+            value="school"
+            onChange={handleCheckboxInput}
+          />
         </div>
 
         <div className={styles.labelContainer}>
           <label>Episodes</label>
-          <input type="number" />
+          <input
+            type="number"
+            name="episodes"
+            value={formState.episodes}
+            onChange={handleInput}
+          />
         </div>
 
         <div className={styles.labelContainer}>
           <label>Plot</label>
-          <textarea value="" />
+          <textarea name="plot" value={formState.plot} onChange={handleInput} />
         </div>
 
         <div className={styles.labelContainer}>
           <label>Main Cast</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="mainCast"
+            value={formState.mainCast}
+            onChange={handleInput}
+          />
         </div>
 
         <div className={styles.labelContainer}>
           <label>Country</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="country"
+            value={formState.country}
+            onChange={handleInput}
+          />
         </div>
 
         <div className={styles.labelContainer}>
           <label>Director</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="director"
+            value={formState.director}
+            onChange={handleInput}
+          />
         </div>
         <div className={styles.labelContainer}>
           <label>Screenwriter</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="screenwriter"
+            value={formState.screenwriter}
+            onChange={handleInput}
+          />
         </div>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
